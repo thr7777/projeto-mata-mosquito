79fc740461ba6ecebfc52233caedf73cd2d10dc7
THR7777 <henrique.dev202100@gmail.com>
2022-09-14 14:42:44 -0300

    atualização do projeto

--- index.js
+++ index.js
@@ -7,47 +7,54 @@ let largura = 0
 function ajusteTamanhoTela(){
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura, largura)
+    
 }
 
 ajusteTamanhoTela()
 
 //===============================================
 // POSIÇÃO RANDOMICA
+
 function posicaoRandomica(){
-    
-let posicaoX = Math.floor(Math.random() * largura) - 90 // atribuir um menos 90 para que a imagem nao saia da tela
-let posicaoY = Math.floor(Math.random() * altura) - 90
+    //apagar elemento anterior
+    //ele basicamente pega o elemento anterior, e apaga para criar um novo, isso se o elemento existir
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+    //============================
+    //POSIÇÃO DA IMAGEM NA TELA ===============================
+    //=========================================================
+let posicaoX = Math.floor(Math.random() * largura) - 150 // atribuir um menos 90 para que a imagem nao saia da tela
+let posicaoY = Math.floor(Math.random() * altura) - 150
 // caso o elemento seja menor que 0 ele vai receber 0, se nao, ele recebe ele mesmo
 //isso serve para que o mosquito ao receber uma posição negativa, o mosquito nao desapareça
 posicaoX = posicaoX < 0 ? 0 : posicaoX
 posicaoY = posicaoY < 0 ? 0 : posicaoY
-
-
-
-console.log(posicaoX, posicaoY)
 
 //criar o elemento html com o dom
 
 let mosquito = document.createElement('img')
 mosquito.src = "imagens/mosca.png"
 mosquito.className = 'imagem_mosquito'
-
+mosquito.id = 'mosquito'
 //atribuição da posicao randomica
 mosquito.style.position = 'absolute'
 mosquito.style.left = posicaoX + 'px'
 mosquito.style.top = posicaoY + 'px'
 mosquito.style.back
 
-//tamanho
+//tamanho randomico do elemento
 let tamanhoA = Math.floor(Math.random() * 150)
 
 tamanhoA = tamanhoA <= 70 ? 70 : tamanhoA 
 mosquito.style.height = tamanhoA + 'px'
 mosquito.style.width = tamanhoA + 'px'
 
+//evento onclick para o mosquito sumir ao clicar
 
-
+mosquito.onclick = function(){
+    this.remove()
+}
 
 // adicionando o elemento dentro do body
 
@@ -57,9 +64,15 @@ document.body.appendChild(mosquito)
 
 mosquito.className = ladoAleatorio()
 }
-posicaoRandomica()
+setInterval(function ()
+{
+    posicaoRandomica()
+}, 2000)
+// setInterval(function(){
+    //funcao
+//}, 100)
 
-// tamanho randomico do mosquito
+
 
 
 //LADO ALEATORIO
@@ -73,6 +86,5 @@ function ladoAleatorio(){
             return 'inverterEsquerda'
     }
 }
-
 
 
